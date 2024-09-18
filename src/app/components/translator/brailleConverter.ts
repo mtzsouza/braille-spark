3efba809aec0573fbb6e7933f22488bcad5bfb1b
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2024-09-18 13:05:44 -0500

    improved braille converter

--- src/app/components/translator/brailleConverter.ts
+++ src/app/components/translator/brailleConverter.ts
@@ -1,61 +1,146 @@
 export function translateToBraille(text: string): string {
     const brailleDict: { [key: string]: string } = {
-        // Latin Characters
-        'a': '⠁', 'b': '⠃', 'c': '⠉', 'd': '⠙', 'e': '⠑',
-        'f': '⠋', 'g': '⠛', 'h': '⠓', 'i': '⠊', 'j': '⠚',
-        'k': '⠅', 'l': '⠇', 'm': '⠍', 'n': '⠝', 'o': '⠕',
-        'p': '⠏', 'q': '⠟', 'r': '⠗', 's': '⠎', 't': '⠞',
-        'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽',
-        'z': '⠵',
-
         // Numbers
-        '1': '⠼⠁', '2': '⠼⠃', '3': '⠼⠉', '4': '⠼⠙', '5': '⠼⠑',
-        '6': '⠼⠋', '7': '⠼⠛', '8': '⠼⠓', '9': '⠼⠊', '0': '⠼⠚',
+        'number-indicator': '⠼', // This character goes before a group of numbers
+        '1': '⠁',
+        '2': '⠃',
+        '3': '⠉',
+        '4': '⠙',
+        '5': '⠑',
+        '6': '⠋',
+        '7': '⠛',
+        '8': '⠓',
+        '9': '⠊',
+        '0': '⠚',
 
+        // Latin Characters
+        'letter-indicator': '⠰',
+        'a': '⠁',
+        'b': '⠃',
+        'c': '⠉',
+        'd': '⠙',
+        'e': '⠑',
+        'f': '⠋',
+        'g': '⠛',
+        'h': '⠓',
+        'i': '⠊',
+        'j': '⠚',
+        'k': '⠅',
+        'l': '⠇',
+        'm': '⠍',
+        'n': '⠝',
+        'o': '⠕',
+        'p': '⠏',
+        'q': '⠟',
+        'r': '⠗',
+        's': '⠎',
+        't': '⠞',
+        'u': '⠥',
+        'v': '⠧',
+        'w': '⠺',
+        'x': '⠭',
+        'y': '⠽',
+        'z': '⠵',
+        
         // Arabic Characters
-        'ا': '⠁', 'ب': '⠃', 'ت': '⠉', 'ث': '⠛', 'ج': '⠙',
-        'ح': '⠑', 'خ': '⠹', 'د': '⠙', 'ذ': '⠑', 'ر': '⠗',
-        'ز': '⠵', 'س': '⠎', 'ش': '⠱', 'ص': '⠡', 'ض': '⠫',
-        'ط': '⠱', 'ظ': '⠳', 'ع': '⠪', 'غ': '⠹', 'ف': '⠋',
-        'ق': '⠟', 'ك': '⠅', 'ل': '⠇', 'م': '⠍', 'ن': '⠝',
-        'ه': '⠓', 'و': '⠺', 'ي': '⠽', 'ة': '⠸⠲', 'أ':'⠌',
-        'إ': '⠨', '؛': '⠰', '،': '⠐', 'آ': '⠜', 'ؤ' : '⠳',
-        'ئ': '⠊', 'ى': '⠕', 
-
-
+        'ا': '⠁',
+        'ب': '⠃',
+        'ت': '⠉',
+        'ث': '⠛',
+        'ج': '⠙',
+        'ح': '⠑',
+        'خ': '⠹',
+        'د': '⠙',
+        'ذ': '⠑',
+        'ر': '⠗',
+        'ز': '⠵',
+        'س': '⠎',
+        'ش': '⠱',
+        'ص': '⠡',
+        'ض': '⠫',
+        'ط': '⠱',
+        'ظ': '⠳',
+        'ع': '⠪',
+        'غ': '⠹',
+        'ف': '⠋',
+        'ق': '⠟',
+        'ك': '⠅',
+        'ل': '⠇',
+        'م': '⠍',
+        'ن': '⠝',
+        'ه': '⠓',
+        'و': '⠺',
+        'ي': '⠽',
+        'ة': '⠸⠲',
+        'أ':'⠌',
+        'إ': '⠨',
+        '؛': '⠰',
+        '،': '⠐',
+        'آ': '⠜',
+        'ؤ': '⠳',
+        'ئ': '⠊',
+        'ى': '⠕',
+        
+        // Symbols
         ' ': '⠀',  // Braille space character
-        '.': '⠲',  // Period
-        ',': '⠂',  // Comma
-        ';': '⠆',  // Semicolon
-        ':': '⠒',  // Colon
-        '!': '⠮',  // Exclamation mark
-        '?': '⠦',  // Question mark
-        "'": '⠄',  // Apostrophe
-        '"': '⠦⠴', // Quotation mark
-        '-': '⠤',  // Hyphen
-        '/': '⠸⠌', // Slash
-        '(': '⠶⠂', // Left parenthesis
-        ')': '⠶⠆', // Right parenthesis
-        '[': '⠦⠶⠂', // Left square bracket
-        ']': '⠦⠶⠆', // Right square bracket
-        '{': '⠦⠶⠲', // Left curly bracket
-        '}': '⠦⠶⠦', // Right curly bracket
-        '&': '⠈⠯',  // Ampersand
-        '*': '⠔',    // Asterisk
-        '_': '⠸⠤',  // Underscore
-        '+': '⠬',    // Plus
-        '=': '⠐⠤',  // Equals
-        '<': '⠦⠣',  // Less than
-        '>': '⠦⠜',  // Greater than
-        '|': '⠦⠳',  // Vertical bar
-        '\\': '⠦⠸⠌', // Backslash
-        '%': '⠸⠴⠦'  // Percent
+        '.': '⠲',
+        ',': '⠂',
+        ';': '⠆',
+        ':': '⠒',
+        '!': '⠮',
+        '?': '⠦',
+        "'": '⠄',
+        '"': '⠦⠴',
+        '-': '⠤',
+        '/': '⠸⠌',
+        '(': '⠶⠂',
+        ')': '⠶⠆',
+        '[': '⠦⠶⠂',
+        ']': '⠦⠶⠆',
+        '{': '⠦⠶⠲',
+        '}': '⠦⠶⠦',
+        '&': '⠈⠯',
+        '*': '⠔',
+        '_': '⠸⠤',
+        '+': '⠬',
+        '=': '⠐⠤',
+        '<': '⠦⠣',
+        '>': '⠦⠜',
+        '|': '⠦⠳',
+        '\\':'⠦⠸⠌', // Backslash
+        '%': '⠸⠴⠦',
     };
     
+    text = text.toLowerCase();
     let brailleText = '';
-    for (const char of text.toLowerCase()) {
-        brailleText += brailleDict[char] || char;  // If character not found, keep it unchanged
+    let ptr: number = 0;
+    while (ptr < text.length) {
+        // If current char is whitespace, add blank character
+        const whitespaceChars = ' \u00A0\u2002\u2003\u2009\u200B\u2029\u000A\u000D\u0085';
+        if (whitespaceChars.includes(text[ptr])) {
+            brailleText += brailleDict[' '];
+            ptr += 1;
+            continue;
+        }
+
+        // If current char is a number, add number indicator
+        if ('0' <= text[ptr] && text[ptr] <= '9') {
+            brailleText += brailleDict['number-indicator'];
+            // Add subsequent numbers
+            while ('0' <= text[ptr] && text[ptr] <= '9') {
+                brailleText += brailleDict[text[ptr]] || text[ptr];
+                ptr += 1;
+            }
+        } else {
+            // Current char is not a number
+            // If previous char was a number, add letter indicator
+            if ('0' <= text[ptr - 1] && text[ptr - 1] <= '9') {
+                brailleText += brailleDict['letter-indicator'];
+            }
+            brailleText += brailleDict[text[ptr]] || text[ptr];
+            ptr += 1;
+        }
     }
-    
+
     return brailleText;
 }
